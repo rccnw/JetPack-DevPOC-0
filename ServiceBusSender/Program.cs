32b1f53619f915c25aa5232f9857c72e6f2a8ae8
Robert Campbell <Robert.Campbell@alaskaair.com>
2018-12-18 13:37:52 -0800

    initial commit

--- ServiceBusSender/Program.cs
+++ ServiceBusSender/Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ServiceBusSender
 {
     class Program
     {
+        const string ServiceBusConnectionString = "Endpoint=sb://jetpack-devpoc-0.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=xGtrXODqlYBXSd2L4anz72XXW99KroL/xVWuhOPSIzs=";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 10;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            await SendMessageAsync(numberOfMessages);
+            await queueClient.CloseAsync();
+        }
+
+
+        static async Task SendMessageAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i=0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the Queue
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending Message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {ex.Message}");
+            }
+
+        }
+
     }
 }
